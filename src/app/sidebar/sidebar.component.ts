fa03838b81e2a912a6150cddd80b75485ced0c3c
Мак <mak@MacBook-Pro-Mak-2.local>
2019-11-06 16:17:05 +0200

    Add sidebar component

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {Folder} from '../services/folder';
+import {FoldersService} from '../services/folders.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+  public foldersList: Folder[] = [];
+
+  constructor(public foldersService: FoldersService) {
+  }
+  ngOnInit() {
+    // request to server
+    // this.foldersService.getData().subscribe((data: { folders: Folder[] }) => {
+    //   this.foldersList = data.folders;
+    // });
+
+  // public getData() {
+  //     this.http.get('/assets/folders.json').subscribe((data: { folders: Folder[] }) =>
+  //       this.subject.next(data.folders));
+  //   }
+    this.foldersService.subject.subscribe(( folders: Folder[] ) => {
+      this.foldersList = folders;
+      // console.log(folders);
+    });
+    this.foldersService.getData();
+  }
+
+  // public open(): void {
+  //   this.router.navigate(['/ContentFolderComponent']);
+  // }
+}
