1656e3ebef06df136c3cf84a7ec0f44f7d089901
Мак <mak@MacBook-Pro-Mak-2.local>
2019-11-06 16:07:16 +0200

    added sidebar-child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import {HeaderComponent} from './header/header.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {FooterComponent} from './footer/footer.component';
 import {ContentFolderComponent} from './content-folder/content-folder.component';
-import { NewFolderComponent } from './new-folder/new-folder.component';
-import { SidebarChildComponent } from './sidebar/sidebar-child/sidebar-child.component';
+import {NewFolderComponent} from './new-folder/new-folder.component';
+import {SidebarChildComponent} from './sidebar/sidebar-child/sidebar-child.component';
 
 @NgModule({
   declarations: [
